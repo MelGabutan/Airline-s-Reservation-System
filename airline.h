d76f5707d5d56d466784ad93abc77a67ac592729
Mel Jefferson T. Gabutan <70356954+MelGabutan@users.noreply.github.com>
2020-08-28 10:25:04 +0800

    Create airline.h

---
+++ airline.h
@@ -0,0 +1,13 @@
+void initSeats(int seats[], int size);
+void displaySeats(int seats[], int size);
+int assignSeat(int st[], int size, int seatType);
+int assignFirstClass(int seats[], int size);
+int assignEconomy(int seats[], int size);
+void boardPass(int seat);
+void Exit(void);
+
+
+// Other Functions
+
+void gotoxy(int row, int col);
+void createBox(int firstRow, int lastRow, int firstColumn, int lastColumn);
